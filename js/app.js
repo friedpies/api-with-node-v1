a5b6b7efe715733873c86231e45a2cb0f6f2054c
friedpies <kenmarut@gmail.com>
2018-06-03 15:37:11 -0500

    Added twit calls to get data, updated auth

--- js/app.js
+++ js/app.js
@@ -4,4 +4,27 @@ const config = require('../config.js');
 
 const app = express();
 
-app.listen(3000);
+const T = new twit(config);
+
+app.set('view engine', 'pug');
+
+
+T.get('statuses/user_timeline', {count: 5}, (err, data) => { // retrieve 5 most recent tweets
+  // console.log(data);
+})
+
+T.get('friends/list', {count: 5}, (err, data) => {
+  // console.log(data);
+})
+
+T.get('direct_messages/events/list', {count: 5}, (err, data) => {
+  console.log(data);
+})
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () =>{
+  console.log('The application is running');
+});
