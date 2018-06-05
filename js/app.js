ffb8f5082963ff377184530b8dbf1be30bbbd81f
friedpies <kenmarut@gmail.com>
2018-06-05 18:27:00 -0500

    added calls for getting user id info, moved some files

--- js/app.js
+++ js/app.js
@@ -9,6 +9,12 @@ const T = new twit(config);
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
+T.get('account/verify_credentials', (err, data) => {
+  const username = data.screen_name;
+  const profileImage = data.profile_image_url;
+  const name = data.name;
+})
+
 T.get('statuses/user_timeline', {count: 5}, (err, data) => { // retrieve 5 most recent tweets
   // console.log(data);
 })
