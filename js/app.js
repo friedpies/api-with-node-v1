0626fff7061c604e4ce656c532749faf81081c03
friedpies <kenmarut@gmail.com>
2018-06-05 21:09:31 -0500

    minor  updates

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ T.get('account/verify_credentials', (err, data) => {
   const username = data.screen_name;
   const profileImage = data.profile_image_url;
   const name = data.name;
+
+  console.log(name + " " + username + " " + profileImage);
 })
 
 T.get('statuses/user_timeline', {count: 5}, (err, data) => { // retrieve 5 most recent tweets
@@ -24,7 +26,7 @@ T.get('friends/list', {count: 5}, (err, data) => {
 })
 
 T.get('direct_messages/events/list', {count: 5}, (err, data) => {
-  console.log(data);
+  // console.log(data);
 })
 
 app.get('/', (req, res) => {
